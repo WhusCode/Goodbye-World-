0bce208073eee22d6482d03c727d6cad99f3cf9e
CalebMartin <camartin370@gmail.com>
2025-09-17 19:39:04 -0400

    Gun Mana/Shooting
    Attempts to merge were made, will update at start of sprint 3

--- Scripts/gun.gd
+++ Scripts/gun.gd
@@ -1,13 +1,17 @@
 extends Area2D
-
+var mana = 50.0
 
 func _physics_process(delta):
 	var enemies_in_range = get_overlapping_bodies()
 	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range.front()
 		look_at(target_enemy.global_position)
-
-
+		
+	if Input.is_action_pressed("BasicMagic") and (mana >= 3):
+		mana -= 3
+		shoot()
+		
+		
 func shoot():
 	const BULLET = preload("res://Scenes/bullet.tscn")
 	var new_bullet = BULLET.instantiate()
@@ -15,6 +19,8 @@ func shoot():
 	new_bullet.global_rotation = %ShootingPoint.global_rotation
 	%ShootingPoint.add_child(new_bullet)
 
+func restoremana():
+	mana += 10
 
-func _on_timer_timeout():
-	shoot()
+func _on_timer_timeout() -> void:
+	restoremana() # Replace with function body.
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,15 +5,12 @@ signal health_depleted
 var health = 100.0
 
 
+
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 600
 	move_and_slide()
-	
-	if velocity.length() > 0.0:
-		%HappyBoo.play_walk_animation()
-	else:
-		%HappyBoo.play_idle_animation()
+
 
 	const DAMAGE_RATE = 5.0
 	var overlapping_mobs = $HurtBox.get_overlapping_bodies()
