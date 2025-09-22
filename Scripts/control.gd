bd3401654795f79cc987c84269b915516ba99e95
CalebMartin <camartin370@gmail.com>
2025-09-22 19:42:21 -0400

    Merge v0.1. Just some small changes
    Someone please for the love of god, fix the movement code, fix the animation code, fix the collision, honestly just redo the whole player. I dont have the energy after merging this.

--- Scripts/control.gd
+++ Scripts/control.gd
@@ -5,7 +5,7 @@ func _process(_delta):
 	pass
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/survivorsgame.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/world.tscn") # Replace with function body.
 
 
 func _on_quit_pressed() -> void:
@@ -13,8 +13,8 @@ func _on_quit_pressed() -> void:
 
 
 func _on_credits_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/917credits.tscn") 
+	get_tree().change_scene_to_file("res://scenes/credits.tscn") 
 
 
 func _on_version_notes_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/917version_notes.tscn") 
+	get_tree().change_scene_to_file("res://scenes/version_notes.tscn") 
