aa06c0b774e9c0614fa9f7f6861b5881eeed064d
emma-alpert <alpert.emma13@gmail.com>
2025-10-01 15:37:54 -0400

    opening narration working!

--- Scripts/opening_narration.gd
+++ Scripts/opening_narration.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
 func _ready():
+	Dialogic.signal_event.connect(_on_dialogic_signal)
 	var opening_narr = Dialogic.start('OpeningNarrTL')
 	add_child(opening_narr)
 
-func _on_timeline_ended(): #CURRENTLY NOT WORKING -- i dont know how to properly do signal code sorryyyy
-	Dialogic.timeline_ended.disconnect(_on_timeline_ended)
-	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+func _on_dialogic_signal(argument: String): #CURRENTLY NOT WORKING -- i dont know how to properly do signal code sorryyyy
+	if argument == "OpeningDone":
+		get_tree().change_scene_to_file("res://Scenes/world.tscn")
