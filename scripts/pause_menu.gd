31bedb198d7cb2ba3abced8caaa08b8806e494a3
CalebMartin <camartin370@gmail.com>
2025-10-04 17:04:36 -0400

    Pause Menu
    I think its done with no textures or anything

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 
-@onready var main = $world.tscn
+@onready var main = $"../../.."
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
