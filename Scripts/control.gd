3adb9d3042f1433aa063f328df2909ae7d409822
Avi <aviiscool047@gmail.com>
2025-10-08 19:24:53 -0400

    Fixed Build issue

--- Scripts/control.gd
+++ Scripts/control.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 		AudioController.play_track_by_file("res://MusicFiles/Does This Sound Minecraft Enough.mp3")
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/world.tscn")
+	get_tree().change_scene_to_file("res://Scenes/world.tscn")
 	AudioController.play_track_by_file("res://MusicFiles/A Thousand Archives, Forever Lost to Time.mp3")
 
 
@@ -19,12 +19,12 @@ func _on_quit_pressed() -> void:
 
 
 func _on_credits_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/credits.tscn") 
+	get_tree().change_scene_to_file("res://Scenes/credits.tscn") 
 
 
 func _on_version_notes_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/version_notes.tscn") 
+	get_tree().change_scene_to_file("res://Scenes/version_notes.tscn") 
 
 
 func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/settings.tscn") 
+	get_tree().change_scene_to_file("res://Scenes/settings.tscn") 
