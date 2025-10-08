0f74a0f786f955d00f1931cdb41ecefecf4bbaaa
CalebMartin <camartin370@gmail.com>
2025-10-08 17:46:44 -0400

    Revert "Reapply "Merge branch 'DialogicBeginnings' of https://github.com/WhusCode/Goodbye-World- into DialogicBeginnings""
    This reverts commit fb8c637ba4ba44fa934d03105d1b63c279dd46fa.

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,27 +1,11 @@
 extends Node2D
 
-@onready var pause_menu = $player/PauseMenu
-var paused = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Global.PlayerInput = false
-	Dialogic.signal_event.connect(_on_dialogic_signal)
-	var opening_narr = Dialogic.start('OpeningNarrTL')
-	add_child(opening_narr)
+	pass # Replace with function body.
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("pause"):
-		pauseMenu()
-		
-func pauseMenu():
-	if paused:
-		pause_menu.hide()
-		Engine.time_scale = 1
-	else:
-		pause_menu.show()
-		Engine.time_scale = 0
-	paused = !paused
-
-func _on_dialogic_signal(argument: String): #CURRENTLY NOT WORKING -- i dont know how to properly do signal code sorryyyy
-	if argument == "OpeningDone":
-		Global.PlayerInput = true	
+	pass
