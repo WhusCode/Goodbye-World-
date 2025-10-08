698b36c8db86b0fac13d714a0ad99e09b198f908
CalebMartin <camartin370@gmail.com>
2025-10-08 16:41:50 -0400

    Revert "Merge branch 'DialogicBeginnings' of https://github.com/WhusCode/Goodbye-World- into DialogicBeginnings"
    This reverts commit deb61906a7dfa29d7a9a0a705717723b41766b41, reversing
    changes made to 745dde1f4b675c3392bb02ebc5b5c695f1551851.

--- Scripts/opening_narration.gd
+++ Scripts/opening_narration.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 func _ready():
-	
+	Dialogic.signal_event.connect(_on_dialogic_signal)
+	var opening_narr = Dialogic.start('OpeningNarrTL')
+	add_child(opening_narr)
 
 func _on_dialogic_signal(argument: String): #CURRENTLY NOT WORKING -- i dont know how to properly do signal code sorryyyy
 	if argument == "OpeningDone":
