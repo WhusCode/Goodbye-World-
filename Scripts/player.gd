ca77d09fa723b1c2d775defd56303254077b7233
CalebMartin <camartin370@gmail.com>
2025-10-08 17:53:09 -0400

    Old Version
    Fixed I think

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -21,7 +21,8 @@ func _ready() -> void:
 func _physics_process(delta):
 	if not player_alive:
 		return
-
+	if Global.PlayerInput == false:
+		return	
 	player_movement(delta)
 	enemy_attack()
 	attack()
@@ -33,6 +34,7 @@ func _physics_process(delta):
 		self.queue_free()
 
 func player_movement(delta):
+	
 	velocity = Vector2.ZERO
 
 	if Input.is_action_pressed("right"):
